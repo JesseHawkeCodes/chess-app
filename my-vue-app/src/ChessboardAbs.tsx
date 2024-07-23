d4afe4c2ca202a2b457721d323d94a62b4df677f
Jesse Hawke <jesse.hawke1@gmail.com>
2024-07-23 12:23:11 +1000

    Mouse reading events now know what square is being clicked and highlight clicked squares.

--- my-vue-app/src/ChessboardAbs.tsx
+++ my-vue-app/src/ChessboardAbs.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import './ChessboardAbs.css'
 
 export default function ChessboardAbs() {
 
+
+
+
+  // Game State
   const [perspective, setPerspective] = useState("white");
 
   const [gameState, setGameState] = useState(
@@ -26,10 +30,124 @@ export default function ChessboardAbs() {
     }
   };
 
+
+
+
+
+  // Highlighting Squares
+  const [highlightedSquareList, setHighlightedSquareList] = useState<Array<HighlightedSquare>>([]);
+
+  // const updateHighlightedSquares = useCallback((action: string, id: number, object: HighlightedSquare) => {
+  //   const temp = highlightedSquareList;
+  //   console.log("Adding new square");
+  //   switch(action){
+  //     case ("add"):
+  //       temp.push(object);
+  //       setHighlightedSquareList(temp); 
+  //       break;
+  //     default:
+  //   }
+  //   console.log("Added new square");
+  //   console.log(highlightedSquareList);
+  // }, [highlightedSquareList]);
+
+  const updateHighlightedSquares = useCallback((action: string, id: number, object: HighlightedSquare) => {
+    // const temp = JSON.parse(JSON.stringify(highlightedSquareList));
+    // console.log("Adding new square");
+
+    console.log("List before set:");
+    console.log(highlightedSquareList);
+
+    switch(action){
+      case ("add"):
+        // console.log("Temp before push:");
+        // console.log(temp);
+
+        // temp.push(object);
+        
+        // console.log("Temp after push:");
+        // console.log(temp);
+
+        setHighlightedSquareList([...highlightedSquareList, object]);
+        // setHighlightedSquareList([object, object, object]);
+
+        console.log("List after set:");
+        console.log(highlightedSquareList);
+        break;
+      default:
+    }
+    // console.log("Added new square");
+    // console.log(highlightedSquareList);
+  }, [highlightedSquareList]);
+
+
+  // Event handler
+  // const mouseEventHandler = useCallback((event: MouseEvent) => {
+  //   // console.log("Mouse event detected");
+  //   try {
+  //     const target = event.currentTarget! as HTMLDivElement;
+  //     const rect = target.getBoundingClientRect();
+  //     const x = event.clientX - rect.left; //x position within the board
+  //     const y = event.clientY - rect.top;  //y position within the board
+  
+  //     const newSquare = {
+  //       id:8,
+  //       column: (Math.floor(x/100)),
+  //       row: (Math.floor(y/100))
+  //     }
+  
+  //     updateHighlightedSquares("add", 8, newSquare);
+
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  
+  // },[updateHighlightedSquares])
+
+  const mouseEventHandler = useCallback((event: MouseEvent) => {
+    // console.log("Mouse event detected");
+    try {
+      const target = event.currentTarget! as HTMLDivElement;
+      const rect = target.getBoundingClientRect();
+      const x = event.clientX - rect.left; //x position within the board
+      const y = event.clientY - rect.top;  //y position within the board
+  
+      const newSquare = {
+        id:8,
+        column: (Math.floor(x/100)),
+        row: (Math.floor(y/100))
+      }
+
+      // console.log("Before addign square:");
+      // console.log(highlightedSquareList);
+      updateHighlightedSquares("add", 8, newSquare);
+
+    } catch (error) {
+      console.log(error);
+    }
+  
+  },[updateHighlightedSquares])
+
+  useEffect(() => {
+
+    document.getElementById("gameBoard")?.addEventListener('click', mouseEventHandler);
+
+    return () => { 
+      document.getElementById("gameBoard")?.removeEventListener('click', mouseEventHandler);
+    };
+
+  }, [mouseEventHandler]);
+
+
+  useEffect(() => {
+    console.log("HS Squre changedd!");
+    console.log(highlightedSquareList);
+  }, [highlightedSquareList])
+
   return (
     <>
       <p> Hello world! </p>
-
+      {console.log("rerender")}
       <button onClick={() => togglePerspective()} >Switch Side</button>
       <p>You are playing from {perspective} perspective</p>
       
@@ -37,24 +155,20 @@ export default function ChessboardAbs() {
 
         <BoardTiles perspective = {perspective} />
 
-        <BoardCoordinates perspective = {perspective} />
+        <HighlightedTiles HighlightedSquares = {highlightedSquareList}/>
+
+        <BoardCoordinates perspective = {perspective}/>
 
         <DisplayPieces gameState = {gameState}></DisplayPieces>
 
-        {/* <InteractionLayer></InteractionLayer> */}
       </div>
     </>
   )
 
 }
 
-function InteractionLayer(){
-  return(
-    <>
-    </>
-  );
-}
 
+//Functional Components
 interface perspectiveProps {
   perspective: string;
 }
@@ -234,6 +348,33 @@ function BoardTiles({perspective} : perspectiveProps) {
   }
 }
 
+interface HighlightedSquaresProps {
+  HighlightedSquares: HighlightedSquare[];
+}
+
+
+function HighlightedTiles({HighlightedSquares} : HighlightedSquaresProps) {
+  const rowClasses = ["highlightRow1","highlightRow2","highlightRow3","highlightRow4","highlightRow5","highlightRow6","highlightRow7","highlightRow8"];
+  const colClasses = ["highlightCol1","highlightCol2","highlightCol3","highlightCol4","highlightCol5","highlightCol6","highlightCol7","highlightCol8"];
+  let highlightClassname = "";
+  console.log("Rendering Highlights");
+  console.log(HighlightedSquares);
+  HighlightedSquares
+  
+  return (
+    <>
+      {HighlightedSquares.map((item) => {
+        highlightClassname = "highlightedSquare" + " " + rowClasses[7 - item.row] + " " + colClasses[item.column];
+        // console.log("Creating highlight");
+        // console.log(item);
+        return (
+          <div className={highlightClassname}/>
+        )
+      })}
+    </>
+  );
+}
+
 function BoardCoordinates({perspective} : perspectiveProps) {
   if (perspective == "white") {
     return(
@@ -346,6 +487,12 @@ function BoardCoordinates({perspective} : perspectiveProps) {
   }
 }
 
+interface HighlightedSquare {
+  id: number;
+  column: number;
+  row: number;
+}
+
 interface gameStateProps {
   gameState: string[][];
 }
@@ -381,24 +528,3 @@ function DisplayPieces({gameState} : gameStateProps) {
     </>
   );
 }
-
-function mouseEventHandler(event: MouseEvent) {
-
-  //Get mouse position inside board and send to console
-  try {
-    const target = event.currentTarget! as HTMLDivElement;
-    const rect = target.getBoundingClientRect();
-    const x = event.clientX - rect.left; //x position within the element.
-    const y = event.clientY - rect.top;  //y position within the element.
-    console.log("Left? : " + x + " ; Top? : " + y + ".");
-  
-  } catch (e) {
-    console.log(e);
-  }
-
-}
-
-window.onload=function(){
-  document.getElementById("gameBoard")?.addEventListener("click", mouseEventHandler);
-}
-
